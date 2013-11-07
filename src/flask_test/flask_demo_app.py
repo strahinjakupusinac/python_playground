7ecc5dcec7b3470b2f6e3d582ba204f721164d77
Strahinja Kupusinac <strahinjakupusinac@gmail.com>
2013-11-07 16:41:29 +0100

    Added authentication experiment.

--- src/flask_test/flask_demo_app.py
+++ src/flask_test/flask_demo_app.py
@@ -4,24 +4,51 @@ Created on Oct 31, 2013
 
 @author: Strahinja
 '''
-from flask import Flask, jsonify, request, abort
+from functools import wraps
+from flask import Flask, jsonify, request, abort, Response, make_response
 
 app = Flask(__name__)
 
 names = []
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'admin'
+
+def authenticate():
+    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 401)
+#     return jsonify({'bla':'bla'}, status=401)
+#     """Sends a 401 response that enables basic auth"""
+#     return Response(
+#     'Could not verify your access level for that URL.\n'
+#     'You have to login with proper credentials', 401,
+#     {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+  
 @app.route("/names", methods=["GET"])
 def getNames():
     return jsonify({"names":names})
 
 
 @app.route("/names", methods=["PUT"])
+@requires_auth
 def addName():
     names.append(request.json)
     return jsonify({"result":"ok"})
 
 
 @app.route("/forward", methods=["POST"])
+@requires_auth
 def forward():
     if not request.json:
         abort(400)
--- test/flask_test/test_flask_demo.py
+++ test/flask_test/test_flask_demo.py
@@ -7,10 +7,16 @@ Created on Oct 31, 2013
 import unittest
 import src.flask_test.flask_demo_app
 import json
+import base64
 
 class FlaskTestCase(unittest.TestCase):
 
 
+    def make_headers(self, username, password):
+        auth = base64.b64encode(username + ':' + password)
+        return {'Content-Type':'application/json', 'Authorization':b'Basic ' + auth} 
+    
+    
     def setUp(self):
         src.flask_test.flask_demo_app.app.config['TESTING'] = True
         self.app = src.flask_test.flask_demo_app.app.test_client()
@@ -24,7 +30,8 @@ class FlaskTestCase(unittest.TestCase):
     def testAddName(self):
         nameToAdd = 'Pera'
         expectedResult = {'result':'ok'}
-        result = json.loads(self.app.put('/names', headers={'Content-Type':'application/json'}, data = json.dumps(nameToAdd)).data)
+        response = self.app.put('/names', headers={'Content-Type':'application/json'}, data = json.dumps(nameToAdd))
+        result = json.loads(response.data)
         self.assertEqual(expectedResult, result)
         expectedResult = {'names':['Pera']}
         result = json.loads(self.app.get('/names').data)
@@ -32,12 +39,16 @@ class FlaskTestCase(unittest.TestCase):
 
 
     def testForward(self):
+        username = 'admin'
+        password = 'admin'
         testJson = {'person':{'firstNam':'Strahinja', 'lastName':'Kupusinac', 'addresses':[u'Bulevar Jovana Dučića 39g, Novi Sad', u'ЈНА 65 Товаришево']}}
         print(testJson)
-        response = self.app.post('/forward', headers={'Content-Type':'application/json'}, data = json.dumps(testJson))
+        headers = self.make_headers(username,password)
+        response = self.app.post('/forward', headers=headers, data = json.dumps(testJson))
         self.assertEqual(200, response.status_code)
         resultJson = json.loads(response.data)
         self.assertEqual(testJson, resultJson)
+
             
 if __name__ == '__main__':
     #import sys;sys.argv = ['', 'Test.testName']
