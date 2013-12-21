8cf5673aa1b049f343babf0ba7fb9cef7d518108
Strahinja Kupusinac <strahinjakupusinac@gmail.com>
2013-12-21 12:55:35 +0100

    Setting up logging

--- src/flask_test/flask_demo_app.py
+++ src/flask_test/flask_demo_app.py
@@ -5,11 +5,12 @@ Created on Oct 31, 2013
 @author: Strahinja
 '''
 from functools import wraps
-from flask import Flask, jsonify, request, abort, Response, make_response
+from flask import Flask, jsonify, request, abort, make_response
 
 app = Flask(__name__)
 
 names = []
+
 
 def check_auth(username, password):
     """This function is called to check if a username /
@@ -17,14 +18,10 @@ def check_auth(username, password):
     """
     return username == 'admin' and password == 'admin'
 
+
 def authenticate():
-    return make_response(jsonify( { 'error': 'Unauthorized access' } ), 401)
-#     return jsonify({'bla':'bla'}, status=401)
-#     """Sends a 401 response that enables basic auth"""
-#     return Response(
-#     'Could not verify your access level for that URL.\n'
-#     'You have to login with proper credentials', 401,
-#     {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 
 def requires_auth(f):
     @wraps(f)
@@ -34,17 +31,18 @@ def requires_auth(f):
             return authenticate()
         return f(*args, **kwargs)
     return decorated
-  
+
+
 @app.route("/names", methods=["GET"])
 def getNames():
-    return jsonify({"names":names})
+    return jsonify({"names": names})
 
 
 @app.route("/names", methods=["PUT"])
 @requires_auth
 def addName():
     names.append(request.json)
-    return jsonify({"result":"ok"})
+    return jsonify({"result": "ok"})
 
 
 @app.route("/forward", methods=["POST"])
@@ -53,7 +51,7 @@ def forward():
     if not request.json:
         abort(400)
     return jsonify(request.json)
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
